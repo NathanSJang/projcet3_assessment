b32d45bfbd9327e6976f0ba27b35163fb48f51ff
Jang <sbtan1989@gmail.com>
2021-01-08 11:14:15 -0800

    Second commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Widget(models.Model):
+    description = models.CharField(max_length=100)
+    quanitity = models.IntegerField()
+
+    def __str__(self):
+        return self.description
+
+    def get_absolute_url(self):
+        return reverse('index')
